6bead7aec2f0c9f4593ea23855e65684aee4819d
snath99920 <srikutan@gmail.com>
2020-10-11 12:23:45 +0530

    Homescreen completed

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
             borderRadius: BorderRadius.circular(5.0),
           ),
           child: Image.asset(
-            'assets/images/p3.jpg',
+            'assets/images/p3.jpeg',
             // height: 20,
             // width: 60,
             fit: BoxFit.cover,
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
           borderRadius: BorderRadius.circular(5.0),
           // ),
           child: Image.asset(
-            'assets/images/p1.jpg',
+            'assets/images/p1.jpeg',
             // height: 50,
             // width: 50,
             fit: BoxFit.cover,
@@ -60,6 +60,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: appBar(context),
@@ -70,11 +71,284 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(
               height: height * 0.05,
             ),
             image_carousel,
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Card(
+              // elevation: 3.0,
+              color: Colors.white,
+              // child: Padding(
+              // padding: EdgeInsets.all(8.0),
+              child: Container(
+                width: width,
+                child: Image.asset(
+                  'assets/images/top.jpg',
+                  // height: 00,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      width: width * 0.45,
+                      height: 250,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 5.0, left: 5.0, right: 5.0),
+                            child: Image.asset(
+                              'assets/images/m1.jpeg',
+                              height: 245,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Color Block, Striped",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Rs 499",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      width: width * 0.45,
+                      height: 250,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 5.0, left: 5.0, right: 5.0),
+                            child: Image.asset(
+                              'assets/images/m3.jpeg',
+                              height: 245,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Printed, Geometric",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Rs 429",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      width: width * 0.45,
+                      height: 250,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 5.0, left: 5.0, right: 5.0),
+                            child: Image.asset(
+                              'assets/images/m4.jpeg',
+                              height: 245,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Casual Petal",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Rs 349",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: Container(
+                      width: width * 0.45,
+                      height: 250,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 5.0, left: 5.0, right: 5.0),
+                            child: Image.asset(
+                              'assets/images/m2.jpeg',
+                              height: 245,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Solid, Hooded",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(
+                                    "Rs 799",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height * 0.02,
+            ),
+            Text(
+              ".....",
+              style: TextStyle(fontSize: 25.0, color: Colors.grey[400]),
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Text(
+              "\" That's all folks..!\"",
+              style: TextStyle(
+                  color: Colors.grey[400],
+                  fontSize: 25.0,
+                  fontStyle: FontStyle.italic),
+            ),
+            SizedBox(
+              height: height * 0.05,
+            ),
           ],
         ),
       ),
